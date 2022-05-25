8fa953629ad6e1b35d81a64c064088b8bcd0ae9a
Matias Parache <mparache@emocha.com>
2022-05-25 20:30:45 -0300

    [add] phantom wallet basic connection

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
+import React, { useEffect } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'falanges';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
+  const checkIfWalletIsConnected = async () => {
+    try {
+    const { solana } = window;
+
+    if (solana) {
+      if (solana.isPhantom) {
+        console.log('Phantom wallet found!');
+      }
+    } else {
+      alert('Solana object not found! Get a Phantom Wallet 👻');
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    };
+    window.addEventListener('load', onLoad);
+  }, []);
+
   return (
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">🖼 POTUS Portal</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            View your POTUS collection in the metaverse ✨
           </p>
         </div>
         <div className="footer-container">
