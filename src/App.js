56f2a373121bc362d6c5b8f1fbcfbd82287aa21e
Matias Parache <mparache@emocha.com>
2022-05-26 22:18:16 -0300

    [add] potus gif and auth conditional render

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import React, { useEffect } from 'react';
+
+import React, { useEffect, useState } from 'react';
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
 const TWITTER_HANDLE = 'falanges';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+const TEST_POTUS = [
+  'https://media.giphy.com/media/l46C8WWpsMtQUDDP2/giphy.gif',
+  'https://media.giphy.com/media/P6Pt8lZw7BiQo/giphy.gif',
+  'https://media.giphy.com/media/l4FGCdurj53YWZYYg/giphy.gif',
+  'https://media.giphy.com/media/3oz8xzkGXMXijbwYA8/giphy.gif'
+];
 
 const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+
   const checkIfWalletIsConnected = async () => {
     try {
     const { solana } = window;
@@ -14,6 +23,12 @@ const App = () => {
     if (solana) {
       if (solana.isPhantom) {
         console.log('Phantom wallet found!');
+        const response = await solana.connect({ onlyIfTrusted: true });
+        console.log(
+          'Connected with Public Key:',
+          response.publicKey.toString()
+        );
+        setWalletAddress(response.publicKey.toString());
       }
     } else {
       alert('Solana object not found! Get a Phantom Wallet 👻');
@@ -23,21 +38,56 @@ const App = () => {
   }
 };
 
+
+  const connectWallet = async () => {
+    const { solana } = window;
+
+    if (solana) {
+      const response = await solana.connect();
+      console.log('Connected with Public Key:', response.publicKey.toString());
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button
+    className="cta-button connect-wallet-button"
+    onClick={connectWallet}
+    >
+      Connect to Wallet
+    </button>
+  );
+
+  const renderConnectedContainer = () => (
+    <div className="connected-container">
+      <div className="gif-grid">
+      {TEST_POTUS.map(gif => (
+        <div className="gif-item" key={gif}>
+          <img src={gif} alt={gif} />
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+
   useEffect(() => {
     const onLoad = async () => {
       await checkIfWalletIsConnected();
     };
     window.addEventListener('load', onLoad);
+    return () => window.removeEventListener('load', onLoad);
   }, []);
 
   return (
     <div className="App">
-      <div className="container">
+      <div className={walletAddress ? 'authed-container' : 'container'}>
         <div className="header-container">
           <p className="header">🖼 POTUS Portal</p>
           <p className="sub-text">
             View your POTUS collection in the metaverse ✨
           </p>
+          {!walletAddress && renderNotConnectedContainer() }
+          {walletAddress && renderConnectedContainer() }
         </div>
         <div className="footer-container">
           <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
